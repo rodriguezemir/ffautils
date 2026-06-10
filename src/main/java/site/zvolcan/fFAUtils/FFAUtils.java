4e3b4558ce74dcccc26017e228d81f6b088c7e31
rodriguezemir <emir49499141@gmail.com>
2026-06-10 20:42:49 -0300

    feat: enhance inventory interactions with sound effects and improve MiniMessage formatting

--- src/main/java/site/zvolcan/fFAUtils/FFAUtils.java
+++ src/main/java/site/zvolcan/fFAUtils/FFAUtils.java
@@ -15,93 +15,97 @@ import fr.mrmicky.fastinv.FastInvManager;
 
 public class FFAUtils extends JavaPlugin {
 
-    @Getter
-    private static FFAUtils instance;
+        @Getter
+        private static FFAUtils instance;
 
-    @Getter
-    private PluginUtils utils;
-    @Getter
-    private SpawnManager spawnManager;
-    @Getter
-    private KitManager kitManager;
-    @Getter
-    private CombatLogManager combatLogManager;
-    @Getter
-    private LobbyManager lobbyManager;
-    @Getter
-    private PlayersManager playersManager;
-    @Getter
-    private DeathEventManager deathEventManager;
-    @Getter
-    private CommandManager commandManager;
-    @Getter
-    private StatsManager statsManager;
-    @Getter
-    private FFAPlaceholders ffaPlaceholders;
-    @Getter
-    private MessagesManager messagesManager;
-    @Getter
-    private ConfigMenuManager configMenuManager;
+        @Getter
+        private PluginUtils utils;
+        @Getter
+        private SpawnManager spawnManager;
+        @Getter
+        private KitManager kitManager;
+        @Getter
+        private CombatLogManager combatLogManager;
+        @Getter
+        private LobbyManager lobbyManager;
+        @Getter
+        private PlayersManager playersManager;
+        @Getter
+        private DeathEventManager deathEventManager;
+        @Getter
+        private CommandManager commandManager;
+        @Getter
+        private StatsManager statsManager;
+        @Getter
+        private FFAPlaceholders ffaPlaceholders;
+        @Getter
+        private MessagesManager messagesManager;
+        @Getter
+        private ConfigMenuManager configMenuManager;
 
-    @Override
-    public void onEnable() {
-        instance = this;
+        @Override
+        public void onEnable() {
+                instance = this;
 
-        getLogger().info("████████████████████████████████████████████████\r\n" + //
-                        "█▄─▄▄─█▄─▄▄─██▀▄─██▄─██─▄█─▄─▄─█▄─▄█▄─▄███─▄▄▄▄█\r\n" + //
-                        "██─▄████─▄████─▀─███─██─████─████─███─██▀█▄▄▄▄─█\r\n" + //
-                        "▀▄▄▄▀▀▀▄▄▄▀▀▀▄▄▀▄▄▀▀▄▄▄▄▀▀▀▄▄▄▀▀▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀");
+                getLogger().info("████████████████████████████████████████████████\r\n" + //
+                                "█▄─▄▄─█▄─▄▄─██▀▄─██▄─██─▄█─▄─▄─█▄─▄█▄─▄███─▄▄▄▄█\r\n" + //
+                                "██─▄████─▄████─▀─███─██─████─████─███─██▀█▄▄▄▄─█\r\n" + //
+                                "▀▄▄▄▀▀▀▄▄▄▀▀▀▄▄▀▄▄▀▀▄▄▄▄▀▀▀▄▄▄▀▀▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀");
 
-        saveDefaultConfig();
-        utils = new PluginUtils(this,
-                YamlConfiguration.loadConfiguration(new File(getDataFolder(), "messages.yml")).getString(
-                        "messages-prefix",
-                        "<b><gradient:#5472F4:#27A2C1>FFAUTILS</gradient></b> <dark_gray>▶️</dark_gray> "));
-        spawnManager = new SpawnManager(this);
-        spawnManager.registerSpawns();
-        getLogger().info("§bLoading Spawns");
-        kitManager = new KitManager(this);
-        kitManager.registerKits();
-        getLogger().info("§bLoading Kits");
-        FastInvManager.register(this);
-        configMenuManager = new ConfigMenuManager(spawnManager, kitManager);
-        getLogger().info("§bLoading ConfigMenu");
-        combatLogManager = new CombatLogManager(this, getConfig().getLong("combatlog.timeout-ticks", 300L));
-        combatLogManager.startCleanupTask();
-        getLogger().info("§bLoading CombatLog");
-        lobbyManager = new LobbyManager(this);
-        getLogger().info("§bLoading LobbyManager");
-        playersManager = new PlayersManager();
-        getLogger().info("§bLoading PlayersManager");
-        statsManager = new StatsManager(this);
-        statsManager.init();
-        getLogger().info("§bLoading StatsManager");
-        messagesManager = new MessagesManager(this);
-        messagesManager.registerMessages();
-        getLogger().info("§bLoading Messages");
-        if (getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
-            ffaPlaceholders = new FFAPlaceholders(this, statsManager);
-            ffaPlaceholders.register();
-        }
-        commandManager = new CommandManager(this, kitManager, spawnManager, lobbyManager, ffaPlaceholders,
-                playersManager, configMenuManager);
-        getLogger().info("§bLoading Commands");
+                saveDefaultConfig();
+                utils = new PluginUtils(this,
+                                YamlConfiguration.loadConfiguration(new File(getDataFolder(), "messages.yml"))
+                                                .getString(
+                                                                "messages-prefix",
+                                                                "<b><gradient:#5472F4:#27A2C1>FFAUTILS</gradient></b> <dark_gray>⯮</dark_gray> "));
+                spawnManager = new SpawnManager(this);
+                spawnManager.registerSpawns();
+                getLogger().info("§bLoading Spawns");
+                kitManager = new KitManager(this);
+                kitManager.registerKits();
+                getLogger().info("§bLoading Kits");
+                FastInvManager.register(this);
+                configMenuManager = new ConfigMenuManager(spawnManager, kitManager);
+                getLogger().info("§bLoading ConfigMenu");
+                combatLogManager = new CombatLogManager(this, getConfig().getLong("combatlog.timeout-ticks",
+                                getConfig().getLong("duration-combat-log", 15) * 20L));
+                combatLogManager.startCleanupTask();
+                getLogger().info("§bLoading CombatLog");
+                lobbyManager = new LobbyManager(this);
+                getLogger().info("§bLoading LobbyManager");
+                playersManager = new PlayersManager();
+                getLogger().info("§bLoading PlayersManager");
+                statsManager = new StatsManager(this);
+                statsManager.init();
+                getLogger().info("§bLoading StatsManager");
+                messagesManager = new MessagesManager(this);
+                messagesManager.registerMessages();
+                getLogger().info("§bLoading Messages");
+                if (getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+                        ffaPlaceholders = new FFAPlaceholders(this, statsManager);
+                        ffaPlaceholders.register();
+                }
+                commandManager = new CommandManager(this, kitManager, spawnManager, lobbyManager, ffaPlaceholders,
+                                playersManager, configMenuManager);
+                getLogger().info("§bLoading Commands");
                 getServer().getPluginManager().registerEvents(
-                new PlayerConnectListener(this, lobbyManager, playersManager, spawnManager, statsManager), this);
-        getServer().getPluginManager().registerEvents(lobbyManager, this);
+                                new PlayerConnectListener(this, lobbyManager, playersManager, spawnManager,
+                                                statsManager),
+                                this);
+                getServer().getPluginManager().registerEvents(lobbyManager, this);
 
-        deathEventManager = new DeathEventManager(this);
-        saveResource("death-messages.yml", false);
-        deathEventManager.registerDeathMessages();
-        getServer().getPluginManager().registerEvents(
-                new PlayerDeathListener(deathEventManager, spawnManager, combatLogManager, statsManager,
-                        playersManager),
-                this);
-    }
+                deathEventManager = new DeathEventManager(this);
+                saveResource("death-messages.yml", false);
+                deathEventManager.registerDeathMessages();
+                getServer().getPluginManager().registerEvents(
+                                new PlayerDeathListener(deathEventManager, spawnManager, combatLogManager, statsManager,
+                                                playersManager),
+                                this);
+        }
 
-    @Override
-    public void onDisable() {
-        statsManager.close();
-        messagesManager.saveMessages();
-    }
+        @Override
+        public void onDisable() {
+                statsManager.close();
+                messagesManager.saveMessages();
+        }
 }
--- src/main/java/site/zvolcan/fFAUtils/inventory/SpawnDetailInventory.java
+++ src/main/java/site/zvolcan/fFAUtils/inventory/SpawnDetailInventory.java
@@ -6,6 +6,7 @@ import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -34,23 +35,33 @@ public class SpawnDetailInventory extends FastInv {
             // Spawn name display
             ItemStack nameItem = new ItemStack(Material.NAME_TAG);
             ItemMeta nameMeta = nameItem.getItemMeta();
-            nameMeta.displayName(MiniMessage.miniMessage().deserialize("<white>" + spawnName + "</white>").decoration(TextDecoration.ITALIC, false));
+            nameMeta.displayName(MiniMessage.miniMessage().deserialize("<white>" + spawnName + "</white>")
+                    .decoration(TextDecoration.ITALIC, false));
             nameItem.setItemMeta(nameMeta);
             setItem(NAME_SLOT, nameItem);
 
             // Info item with world and coordinates
             ItemStack infoItem = new ItemStack(Material.PAPER);
             ItemMeta infoMeta = infoItem.getItemMeta();
-            infoMeta.displayName(MiniMessage.miniMessage().deserialize("<gray>Spawn Info</gray>").decoration(TextDecoration.ITALIC, false));
+            infoMeta.displayName(MiniMessage.miniMessage().deserialize("<gray>Spawn Info</gray>")
+                    .decoration(TextDecoration.ITALIC, false));
             List<Component> lore = new ArrayList<>();
-            lore.add(MiniMessage.miniMessage().deserialize("<gray>World: " + (world != null ? world.getName() : "Unknown") + "</gray>").decoration(TextDecoration.ITALIC, false));
-            lore.add(MiniMessage.miniMessage().deserialize("<gray>" + String.format("X: %.1f Y: %.1f Z: %.1f", loc.getX(), loc.getY(), loc.getZ()) + "</gray>").decoration(TextDecoration.ITALIC, false));
+            lore.add(MiniMessage.miniMessage()
+                    .deserialize("<gray>World: " + (world != null ? world.getName() : "Unknown") + "</gray>")
+                    .decoration(TextDecoration.ITALIC, false));
+            lore.add(MiniMessage
+                    .miniMessage().deserialize("<gray>"
+                            + String.format("X: %.1f Y: %.1f Z: %.1f", loc.getX(), loc.getY(), loc.getZ()) + "</gray>")
+                    .decoration(TextDecoration.ITALIC, false));
 
             List<String> allowedKits = spawnManager.getAllowedKits(spawnName);
             if (allowedKits != null && !allowedKits.isEmpty()) {
-                lore.add(MiniMessage.miniMessage().deserialize("<gray>Allowed kits: " + String.join(", ", allowedKits) + "</gray>").decoration(TextDecoration.ITALIC, false));
+                lore.add(MiniMessage.miniMessage()
+                        .deserialize("<gray>Allowed kits: " + String.join(", ", allowedKits) + "</gray>")
+                        .decoration(TextDecoration.ITALIC, false));
             } else {
-                lore.add(MiniMessage.miniMessage().deserialize("<gray>Allowed kits: All kits</gray>").decoration(TextDecoration.ITALIC, false));
+                lore.add(MiniMessage.miniMessage().deserialize("<gray>Allowed kits: All kits</gray>")
+                        .decoration(TextDecoration.ITALIC, false));
             }
             infoMeta.lore(lore);
             infoItem.setItemMeta(infoMeta);
@@ -60,8 +71,13 @@ public class SpawnDetailInventory extends FastInv {
         // Back button
         ItemStack backItem = new ItemStack(Material.ARROW);
         ItemMeta backMeta = backItem.getItemMeta();
-        backMeta.displayName(MiniMessage.miniMessage().deserialize("<gray>Back</gray>").decoration(TextDecoration.ITALIC, false));
+        backMeta.displayName(
+                MiniMessage.miniMessage().deserialize("<gray>Back</gray>").decoration(TextDecoration.ITALIC, false));
         backItem.setItemMeta(backMeta);
-        setItem(BACK_SLOT, backItem, e -> configMenuManager.openSpawns((Player) e.getWhoClicked(), previousPage));
+        setItem(BACK_SLOT, backItem, e -> {
+            final Player player = (Player) e.getWhoClicked();
+            player.playSound(player, Sound.UI_BUTTON_CLICK, 1, 1);
+            configMenuManager.openSpawns(player, previousPage);
+        });
     }
 }
